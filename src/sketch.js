9b1dea44a2c3f6aaa5d52ba179d3d5e3859e1a36
Zoe Li <zli59@calpoly.edu>
2024-03-17 16:42:18 -0700

    added animation of scene 5 of the ghost floating.

--- src/sketch.js
+++ src/sketch.js
@@ -3,16 +3,30 @@ let animationSpeed = 2;
 let legsOpen = true;
 rr= 0;
 lr = 0;
+let xStart = [];
+let brickWidth = [];
 
 function setup() {
   // For ordering nodes in the DOM
   let myCanvas = createCanvas(400, 400);
   myCanvas.parent("canvas-parent");
+  for (let i = 0; i < 6; i++) {
+    xStart.push(random(50) * -1);
+    brickWidth.push([]);
+    let x = xStart[i];
+    let brick = 0;
+    while (x <= width) {
+      let w = random(50, 100);
+      brickWidth[i][brick] = w;
+      brick++;
+      x += w;
+    }
+  }
 }
 
 function draw(){
 
-  if (timer > 3 && timer < 400) {
+  if (timer > 3 && timer < 100) {
     drawLandscape();
     drawWall();
     drawHorse(70,270);
@@ -31,7 +45,91 @@ function draw(){
     frameRate(20);
   }
   timer++;
+
+  if (timer > 100 && timer < 200) {
+    drawLandscape();
+    drawStillWall();
+  drawHumpity(150,300,0);
+  drawHumpity(250,300,5);
+  drawGhost();
+  }
   
+}
+
+
+function drawLandscape() {
+  background(135, 206, 250);
+
+  fill(220);
+  rect(0, height / 2, width, height / 2);
+  
+}
+
+function drawStillWall(){
+  let y = 100;
+  let rowNum = 0;
+  while (y <= 250){
+    drawRow(y, rowNum);
+    y += 30;
+    rowNum++;
+  }
+}
+
+// brick row
+function drawStillRow(y, rowNum){
+  let x = xStart[rowNum];
+  let brick = 0;
+  while (x <= width) {
+    let w = brickWidth[rowNum][brick];
+    drawBrick(x, y, w);
+    x += w;
+    brick++;
+  }
+}
+
+// brick
+function drawStillBrick(x, y, w) {
+  push();
+  translate(x, y);
+  fill(139, 69, 19);
+  rect(0, 0, w, 30);
+  pop();
+}
+
+function drawHumpity(eggx, eggy, r){
+  push();
+  translate(eggx, eggy);
+  rotate(r);
+  noStroke();
+  fill (255);
+  arc(0, 0, 100, 100, PI/4, PI*5/4);
+  pop();
+}
+
+let gy = 0;
+let targetGy = 0;
+
+function drawGhost(){
+  push();
+  translate(200, 200);
+  fill (255);
+  triangle(-50,gy,50,gy,0,gy+100)
+  ellipse(0, gy, 100);
+  noFill()
+  line(20,gy+10,-20,gy-10)
+  line(-20,gy+10,20,gy-10)
+  pop();
+  
+  if (gy !== targetGy) {
+    let direction = (targetGy - gy) / abs(targetGy - gy); 
+    gy += direction * animationSpeed; 
+  }
+}
+
+function mouseClicked() {
+  targetGy -= 400; 
+  //loop(); 
+  frameRate(60); 
 }
 
 function drawLandscape() {
